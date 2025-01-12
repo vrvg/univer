fdc480aa29f37df90c9e017815828dd6c8a0d786
vange <ne.huatl@vange.li>
2025-01-12 05:58:05 -0800

    adding packages that go in the root directory of the site

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+module.exports = {
+	entry: './themes/univer/assets/js/index.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'themes','univer','assets','js')
+	}
+};
+
+'use strict'
+
+const path2 = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './themes/univer/assets/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './themes/univer/layouts/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
